8d5e2afda49ef79220c10b9149addc63bf6059f2
Michael Nye <thenyeguy@gmail.com>
2014-02-22 16:57:33 -0500

    Removed extra line in android.mk

--- Android.mk
+++ Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 # Set compiler flags
-#LOCAL_CFLAGS += -std=c++11 -Wall -Wno-delete-non-virtual-dtor -Werror -g
 LOCAL_CPPFLAGS := -std=c++11 -pthread -frtti -fexceptions -g \
 	-Wall -Werror -Wno-sign-compare -Wno-maybe-uninitialized
 
