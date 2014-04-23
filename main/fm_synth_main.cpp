9c252edc60c2053c08ef045d18bf5698444d2fbd
Michael Nye <thenyeguy@gmail.com>
2014-04-22 21:18:18 -0400

    Added skeleton FM synth

--- main/fm_synth_main.cpp
+++ main/fm_synth_main.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 #include "../src/clip_detector.h"
+#include "../src/fm_synth.h"
 #include "../src/limiter.h"
 #include "../src/midi_wrapper.h"
 #include "../src/oscillator.h"
 #include "../src/speaker.h"
-#include "../src/subtractive_synth.h"
 
 int main()
 {
     using namespace std;
 
     cout << "Initializing MIDI instrument" << endl;
-    // SubtractiveSynth inst(10);
-    // MidiListener midi(&inst, 1);
+    FMSynth inst(10);
+    MidiListener midi(&inst, 1);
 
-    Oscillator carrier(Oscillator::Sine, 440);
-    Oscillator mod(Oscillator::Saw, 440);
-    
-    carrier.set_modulator_input(mod.get_output_channel());
-    carrier.set_modulator_intensity(20);
+    inst.set_modulator_intensity(5);
 
     cout << "Initializing signal chain" << endl;
     Limiter limiter(-3.0);
-    limiter.set_input_channel(carrier.get_output_channel());
+    limiter.set_input_channel(inst.get_output_channel());
 
     ClipDetector clip(1.0);
     clip.set_input_channel(limiter.get_output_channel());
 
     Speaker out;
     out.set_input_channel(clip.get_output_channel());
-    // out.register_callback(MidiListener::timing_callback, &midi);
+    out.register_callback(MidiListener::timing_callback, &midi);
 
     cout << "Entering playback loop..." << endl << endl;
     while(true)
---
+++ src/fm_synth.h
@@ -0,0 +1,107 @@
+#ifndef FM_SYNTH_H
+#define FM_SYNTH_H
+
+#include "adsr.h"
+#include "gain_filter.h"
+#include "oscillator.h"
+#include "polyphonic_instrument.h"
+#include "second_order_filter.h"
+
+
+namespace ClickTrack
+{
+    /* This is a subtractive synthesizer controlled over MIDI. It is polyphonic.
+     */
+    class FMSynthVoice;
+    class FMSynth : public PolyphonicInstrument
+    {
+        public:
+            /* Constructor/destructor. Takes in how many voices to use, as
+             * well as the MIDI channel
+             */
+            FMSynth(int voices=1);
+
+            /* Override because we have added more signal chain
+             */
+            Channel* get_output_channel();
+
+            /* Sets the output gain of the synth, in decibels
+             */
+            void set_gain(float gain);
+
+            /* Voice selection for each oscillator - each oscillator can be one
+             * of several modes as defined in oscillator.h
+             */
+            void set_carrier_mode(Oscillator::Mode mode);
+            void set_modulator_mode(Oscillator::Mode mode);
+
+            /* Transposition selection for each oscillator - transpose in
+             * arbitrary step intervals
+             */
+            void set_carrier_transposition(float steps);
+            void set_modulator_transposition(float steps);
+
+            /* Controls the degree of modulation
+             */
+            void set_modulator_intensity(float intensity);
+
+            /* Setters for the ADSR state
+             */
+            void set_attack_time(float attack_time);
+            void set_decay_time(float decay_time);
+            void set_sustain_level(float sustain_level);
+            void set_release_time(float release_time);
+
+            /* A filter in the signal chain
+             */
+            SecondOrderFilter filter;
+
+            /* The LFO is connected to the oscillators to generate vibrato, and
+             * to the output gain to generate tremolo.
+             *
+             * Vibrato is specified in steps, tremelo in decibal variation
+             */
+            Oscillator lfo;
+            void set_lfo_vibrato(float steps);
+            void set_lfo_tremelo(float db);
+
+        private:
+            /* Output gain for the oscillator. Also performs tremelo
+             */
+            GainFilter volume;
+
+            /* Our list of voices so we may set their parameters
+             */
+            std::vector<FMSynthVoice*> voices;
+    };
+
+
+    class FMSynthVoice : public PolyphonicVoice
+    {
+        friend class FMSynth;
+
+        public:
+            /* Constructor/destructor
+             */
+            FMSynthVoice(FMSynth* parent_synth);
+
+            /* Gets the output of this voice
+             */
+            Channel* get_output_channel();
+
+            /* Callbacks for starting and stopping notes
+             */
+            void handle_note_down(float velocity, unsigned long time);
+            void handle_note_up(unsigned long time);
+            void handle_pitch_wheel(float value, unsigned long time);
+
+        protected:
+            /* Define our signal chain
+             */
+            Oscillator carrier, modulator;
+            ADSRFilter adsr;
+    };
+}
+
+
+#endif
